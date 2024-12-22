533076889dd72d876035c764b20e3c052b46fb6a
Martin Ocando <martinocandop@gmail.com>
2024-12-22 20:28:36 -0300

    refactor(game): Remove unused imports for the grid

--- src/components/organisms/game-grid/game-grid.tsx
+++ src/components/organisms/game-grid/game-grid.tsx
@@ -1,4 +1,5 @@
 import GameCard from "@/components/molecules/game-card";
+import { CartProvider } from "@/contexts/cart-context";
 import { Game } from "@/types";
 
 interface GameGridProps {
@@ -7,10 +8,12 @@ interface GameGridProps {
 
 export default function GameGrid({ games }: GameGridProps) {
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
-      {games.map((game) => (
-        <GameCard key={game.id} game={game} />
-      ))}
-    </div>
+    <CartProvider>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
+        {games.map((game) => (
+          <GameCard key={game.id} game={game} />
+        ))}
+      </div>
+    </CartProvider>
   );
 }
