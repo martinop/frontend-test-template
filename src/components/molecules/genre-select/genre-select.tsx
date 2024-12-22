16e2f449caf63e047424fe4f5371962083f22e85
Martin Ocando <martinocandop@gmail.com>
2024-12-22 17:35:34 -0300

    fix(genre): sync select value with URL params

--- src/components/molecules/genre-select/genre-select.tsx
+++ src/components/molecules/genre-select/genre-select.tsx
@@ -7,6 +7,7 @@ export default function GenreSelect() {
   const { genres } = useGenreContext();
   const router = useRouter();
   const searchParams = useSearchParams();
+  const currentGenre = searchParams.get("genre") || "";
 
   const handleGenreChange = (newGenre: string) => {
     const params = new URLSearchParams(searchParams);
@@ -19,6 +20,8 @@ export default function GenreSelect() {
     router.push(`/?${params.toString()}`);
   };
 
+  console.log({ genres });
+
   return (
     <div className="flex items-center">
       <span className="text-xl text-gray-medium font-bold pr-6 border-r border-stroke-secondary mr-6">
@@ -27,6 +30,7 @@ export default function GenreSelect() {
       <select
         className="w-[230px] px-4 h-14 text-xl text-primary"
         onChange={(e) => handleGenreChange(e.target.value)}
+        value={currentGenre}
       >
         <option value="">All</option>
         {genres.map((genre) => (
