2344db59d1e220da949ac9ac7a6383b5f2b09e9f
Martin Ocando <martinocandop@gmail.com>
2024-12-22 17:05:32 -0300

    chore(styles): add new color tokens to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,15 +9,19 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        "surface-primary": "#FFFFFF",
         "surface-secondary": "#EEEEEE",
         "neutral-dark": "#585660",
+        "neutral-500": "#737373",
         "neutral-700": "#404040",
         "gray-medium": "#3B3B3B",
         // Duplicated (?
         primary: "#3B3B3B",
         "stroke-secondary": "#3B3B3B",
+        "item-fill": "#3B3B3B",
         // End of duplicated by Figma
         "stroke-tertiary": "#EFEDF3",
+        "stroke-alternative": "#8F8F8F",
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
