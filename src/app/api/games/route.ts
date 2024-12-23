20f09175bd7c8589f85ab49f58f020f7dd3d33a4
Martin Ocando <martinocandop@gmail.com>
2024-12-22 22:00:57 -0300

    fix(api): calculate totalPages using filtered games length

--- src/app/api/games/route.ts
+++ src/app/api/games/route.ts
@@ -7,25 +7,24 @@ export async function GET(request: Request) {
   const genre = searchParams.get("genre");
   let page = parseInt(searchParams.get("page") ?? "1");
 
-  let games = allGames;
-
-  if (genre) {
-    games = games.filter(
-      (game) => game.genre.toLowerCase() === genre.toLowerCase()
-    );
-  }
+  let filteredGames = genre
+    ? allGames.filter(
+        (game) => game.genre.toLowerCase() === genre.toLowerCase()
+      )
+    : allGames;
 
   if (page < 1 || isNaN(page)) page = 1;
 
   // Mock a delay to simulate a real API
   await delay(2000);
 
-  const fromIndex = (page - 1) * ITEMS_PER_PAGE;
-  const toIndex = page * ITEMS_PER_PAGE;
-  games = games.slice(fromIndex, toIndex);
-
-  const totalPages = Math.ceil(allGames.length / ITEMS_PER_PAGE);
+  const totalPages = Math.ceil(filteredGames.length / ITEMS_PER_PAGE);
   const currentPage = page;
+
+  // I commented this to get all of the expected items
+  // const fromIndex = (page - 1) * ITEMS_PER_PAGE;
+  const toIndex = page * ITEMS_PER_PAGE;
+  const games = filteredGames.slice(0, toIndex);
 
   return Response.json({ games, availableFilters, totalPages, currentPage });
 }
